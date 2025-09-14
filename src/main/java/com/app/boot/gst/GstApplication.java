5066a57ea9872bf3679b8a18a5944e9eac28e916
tejesh6835 <tejeshankem@gmail.com>
2025-09-14 23:40:00 +0530

    First Commit

---
+++ src/main/java/com/app/boot/gst/GstApplication.java
@@ -0,0 +1,14 @@
+package com.app.boot.gst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GstApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/app/boot/gst/GstapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.boot.gst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GstapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
